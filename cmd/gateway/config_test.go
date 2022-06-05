2f86ee6fd71b9e9af5b6d7521a071ad70fd0b721
liusiyuan <945212191@qq.com>
2022-06-05 17:34:27 +0800

    feat: add cmd

---
+++ cmd/gateway/config_test.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"testing"
+
+	configv1 "github.com/go-kratos/gateway/api/gateway/config/v1"
+	circuitbreakerv1 "github.com/go-kratos/gateway/api/gateway/middleware/circuitbreaker/v1"
+	corsv1 "github.com/go-kratos/gateway/api/gateway/middleware/cors/v1"
+	otelv1 "github.com/go-kratos/gateway/api/gateway/middleware/otel/v1"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+func equalTo() *configv1.Gateway {
+	return &configv1.Gateway{
+		Name:    "helloworld",
+		Version: "v1",
+		Hosts: []string{
+			"localhost",
+			"127.0.0.1",
+		},
+		Endpoints: []*configv1.Endpoint{
+			{
+				Path:     "/helloworld/*",
+				Protocol: configv1.Protocol_HTTP,
+				Timeout:  &durationpb.Duration{Seconds: 1},
+				Backends: []*configv1.Backend{
+					{
+						Target: "127.0.0.1:8000",
+					},
+				},
+				Middlewares: []*configv1.Middleware{
+					{
+						Name: "circuitbreaker",
+						Options: asAny(&circuitbreakerv1.CircuitBreaker{
+							Trigger: &circuitbreakerv1.CircuitBreaker_SuccessRatio{
+								SuccessRatio: &circuitbreakerv1.SuccessRatio{
+									Success: 0.6,
+									Request: 1,
+									Bucket:  10,
+									Window:  &durationpb.Duration{Seconds: 3},
+								},
+							},
+							Action: &circuitbreakerv1.CircuitBreaker_BackupService{
+								BackupService: &circuitbreakerv1.BackupService{
+									Endpoint: &configv1.Endpoint{
+										Backends: []*configv1.Backend{
+											{
+												Target: "127.0.0.1:8001",
+											},
+										},
+									},
+								},
+							},
+							AssertCondtions: []*configv1.Condition{
+								{
+									Condition: &configv1.Condition_ByStatusCode{
+										ByStatusCode: "200",
+									},
+								},
+							},
+						}),
+					},
+				},
+			},
+			{
+				Path:     "/helloworld.Greeter/*",
+				Method:   "POST",
+				Protocol: configv1.Protocol_GRPC,
+				Timeout:  &durationpb.Duration{Seconds: 1},
+				Backends: []*configv1.Backend{
+					{
+						Target: "127.0.0.1:9000",
+					},
+				},
+				Retry: &configv1.Retry{
+					Attempts:      3,
+					PerTryTimeout: &durationpb.Duration{Nanos: 100000000},
+					Conditions: []*configv1.Condition{
+						{Condition: &configv1.Condition_ByStatusCode{ByStatusCode: "502-504"}},
+						{Condition: &configv1.Condition_ByHeader{ByHeader: &configv1.ConditionHeader{
+							Name:  "Grpc-Status",
+							Value: "14",
+						}}},
+					},
+				},
+			},
+		},
+		Middlewares: []*configv1.Middleware{
+			{
+				Name: "logging",
+			},
+			{
+				Name: "transcoder",
+			},
+			{
+				Name: "cors",
+				Options: asAny(&corsv1.Cors{
+					AllowCredentials: true,
+					AllowOrigins:     []string{".google.com"},
+					AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+				}),
+			},
+			{
+				Name: "otel",
+				Options: asAny(&otelv1.Otel{
+					HttpEndpoint: "localhost:4318",
+				}),
+			},
+		},
+	}
+}
+
+func asAny(in proto.Message) *anypb.Any {
+	out, err := anypb.New(in)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+func TestConfigUnmarshaler(t *testing.T) {
+	cfg := config.New(
+		config.WithSource(
+			file.NewSource("config.yaml"),
+		),
+	)
+	if err := cfg.Load(); err != nil {
+		t.Fatal(err)
+	}
+	gateway := &configv1.Gateway{}
+	if err := cfg.Scan(gateway); err != nil {
+		t.Fatal(err)
+	}
+
+	left, err := protojson.Marshal(gateway)
+	if err != nil {
+		t.Fatal(err)
+	}
+	right, err := protojson.Marshal(equalTo())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("gateway config:\nloaded: %s\nshould equal to: %s\n", left, right)
+
+	if !proto.Equal(gateway, equalTo()) {
+		t.Errorf("inconsistent gateway config")
+	}
+}
---
+++ cmd/gateway/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/go-kratos/gateway/client"
+	"github.com/go-kratos/gateway/config"
+	configLoader "github.com/go-kratos/gateway/config/config-loader"
+	"github.com/go-kratos/gateway/discovery"
+	"github.com/go-kratos/gateway/middleware"
+	"github.com/go-kratos/gateway/proxy"
+	"github.com/go-kratos/gateway/proxy/debug"
+	"github.com/go-kratos/gateway/server"
+
+	_ "net/http/pprof"
+
+	_ "github.com/go-kratos/gateway/discovery/nacos"
+	"github.com/go-kratos/gateway/middleware/circuitbreaker"
+	_ "github.com/go-kratos/gateway/middleware/cors"
+	_ "github.com/go-kratos/gateway/middleware/logging"
+	_ "github.com/go-kratos/gateway/middleware/otel"
+	_ "github.com/go-kratos/gateway/middleware/rewrite"
+	_ "github.com/go-kratos/gateway/middleware/transcoder"
+	_ "go.uber.org/automaxprocs"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/registry"
+)
+
+var (
+	ctrlService  string
+	discoveryDSN string
+	proxyAddr    string
+	proxyConfig  string
+	withDebug    bool
+)
+
+var (
+	// LOG .
+	LOG = log.NewHelper(log.With(log.GetLogger(), "source", "main"))
+)
+
+func init() {
+	flag.BoolVar(&withDebug, "debug", false, "enable debug handlers")
+	flag.StringVar(&proxyAddr, "addr", ":8080", "proxy address, eg: -addr 0.0.0.0:8080")
+	flag.StringVar(&proxyConfig, "conf", "config.yaml", "config path, eg: -conf config.yaml")
+	flag.StringVar(&ctrlService, "ctrl.service", "", "control service host, eg: http://127.0.0.1:8000")
+	flag.StringVar(&discoveryDSN, "discovery.dsn", "", "discovery dsn, eg: consul://127.0.0.1:7070?token=secret&datacenter=prod")
+}
+
+func makeDiscovery() registry.Discovery {
+	if discoveryDSN == "" {
+		return nil
+	}
+	d, err := discovery.Create(discoveryDSN)
+	if err != nil {
+		log.Fatalf("failed to create discovery: %v", err)
+	}
+	return d
+}
+
+func main() {
+	flag.Parse()
+
+	clientFactory := client.NewFactory(makeDiscovery())
+	p, err := proxy.New(clientFactory, middleware.Create)
+	if err != nil {
+		LOG.Fatalf("failed to new proxy: %v", err)
+	}
+	circuitbreaker.Init(clientFactory)
+
+	ctx := context.Background()
+	var ctrlLoader *configLoader.CtrlConfigLoader
+	if ctrlService != "" {
+		LOG.Infof("setup control service to: %q", ctrlService)
+		ctrlLoader = configLoader.New(ctrlService, proxyConfig)
+		if err := ctrlLoader.Load(ctx); err != nil {
+			LOG.Errorf("failed to do initial load from control service: %v, using local config instead", err)
+		}
+		go ctrlLoader.Run(ctx)
+	}
+
+	confLoader, err := config.NewFileLoader(proxyConfig)
+	if err != nil {
+		LOG.Fatalf("failed to create config file loader: %v", err)
+	}
+	defer confLoader.Close()
+	bc, err := confLoader.Load(context.Background())
+	if err != nil {
+		LOG.Fatalf("failed to load config: %v", err)
+	}
+
+	if err := p.Update(bc); err != nil {
+		LOG.Fatalf("failed to update service config: %v", err)
+	}
+	reloader := func() error {
+		bc, err := confLoader.Load(context.Background())
+		if err != nil {
+			LOG.Errorf("failed to load config: %v", err)
+			return err
+		}
+		if err := p.Update(bc); err != nil {
+			LOG.Errorf("failed to update service config: %v", err)
+			return err
+		}
+		LOG.Infof("config reloaded")
+		return nil
+	}
+	confLoader.Watch(reloader)
+
+	var serverHandler http.Handler = p
+	if withDebug {
+		debugService := debug.New()
+		debugService.Register("proxy", p)
+		debugService.Register("config", confLoader)
+		if ctrlLoader != nil {
+			debugService.Register("ctrl", ctrlLoader)
+		}
+		serverHandler = debug.MashupWithDebugHandler(debugService, p)
+	}
+	app := kratos.New(
+		kratos.Name(bc.Name),
+		kratos.Context(ctx),
+		kratos.Server(
+			server.NewProxy(serverHandler, proxyAddr, bc),
+		),
+	)
+	if err := app.Run(); err != nil {
+		LOG.Errorf("failed to run servers: %v", err)
+	}
+}
