488d8bf5b9528fd9364af5125875fd90a84ec0d7
liusiyuan <945212191@qq.com>
2022-06-05 17:35:13 +0800

    add: discovery

---
+++ discovery/nacos/nacos.go
@@ -0,0 +1,53 @@
+package nacos
+
+import (
+	"github.com/go-kratos/gateway/discovery"
+	"github.com/go-kratos/kratos/contrib/registry/nacos/v2"
+	_ "github.com/go-kratos/kratos/contrib/registry/nacos/v2"
+	"github.com/go-kratos/kratos/v2/registry"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	_ "github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	_ "github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	_ "github.com/nacos-group/nacos-sdk-go/vo"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+func init() {
+	discovery.Register("nacos", New)
+}
+
+func New(dsn *url.URL) (registry.Discovery, error) {
+	host := strings.Split(dsn.Host, ":")
+	ip := host[0]
+	port, err := strconv.Atoi(host[1])
+	if err != nil {
+		return nil, err
+	}
+
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig(ip, uint64(port)),
+	}
+
+	cc := constant.ClientConfig{
+		UpdateCacheWhenEmpty: true,
+		NamespaceId:          "public",
+		TimeoutMs:            5000,
+	}
+
+	client, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ServerConfigs: sc,
+			ClientConfig:  &cc,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nacos.New(client), nil
+}
