793ce012d1e949ea86e1095dd90eaadb32426b29
liusiyuan <945212191@qq.com>
2022-11-14 10:10:51 +0800

    refactor: update some env set about nacos

--- config/config-loader/nacos-ctrl-loader.go
+++ config/config-loader/nacos-ctrl-loader.go
@@ -57,9 +57,15 @@ func (c *NacosCtrlConfigLoader) load(ctx context.Context) ([]byte, error) {
 		group = "DEFAULT_GROUP"
 	}
 
+	tenant := os.Getenv("NACOS_NAMESPACE")
+	if tenant == "" {
+		tenant = "public"
+	}
+
 	params := url.Values{}
 	params.Set("dataId", dataId)
 	params.Set("group", group)
+	params.Set("tenant", tenant)
 	LOG.Infof("%s is requesting config from %s with params: %+v", c.advertiseName, c.ctrlService, params)
 	api, err := c.urlfor("/nacos/v1/cs/configs", params)
 	if err != nil {
